d70e4f0eb8b315008e24108206e2a38d2396db38
Starvingwolf <119147445+Starvingwolf@users.noreply.github.com>
2022-12-15 20:15:59 +0530

    Update mvsimp.sh

--- mvsimp.sh
+++ mvsimp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "type the name of the file you want to move  "
+echo "Enter the name of the file you want to move  "
 read filename
-echo "type the destination you want to movie the file"
+echo "type the name of the location that your file want to be moved "
 read dest
 sudo find /home -name $dest >> file
 real=$(cat file)
